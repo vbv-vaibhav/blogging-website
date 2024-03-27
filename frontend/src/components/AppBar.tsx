3c2477ec8eb4591889318ab0fcb97cac97c6b9c1
Vaibhav Bansal <vbvbansal@gmail.com>
2024-03-27 16:19:36 +0530

    fixed deployment issues

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { Avatar } from "./BlogCard";
 
 const AppBar = () => {
   return (
---
+++ frontend/src/config.ts
@@ -0,0 +1,1 @@
+export const BACKEND_URL = "https://blogging-website.vbvbansal-5ae.workers.dev"
